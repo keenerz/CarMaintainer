14c025b27857f8a12c118cfb241cd92c9254dfa0
Keenan <kkan68@gmail.com>
2022-03-26 01:26:21 +0000

    started working on info page

--- src/front/js/component/carinfo.js
+++ src/front/js/component/carinfo.js
@@ -1,6 +1,12 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-export const Carinfo = () => {
-  return <div></div>;
+export const Carinfo = (props) => {
+  const { store, actions } = useContext(Context);
+  return (
+    <div>
+      <p>Car Make: {props?.data.make}</p>
+    </div>
+  );
 };
