a4218776f28b2b585baf288041b661f54c6dd6ad
Keenan <kkan68@gmail.com>
2022-03-27 07:31:33 +0000

    added some items to the carinfo component view

--- src/front/js/component/carinfo.js
+++ src/front/js/component/carinfo.js
@@ -6,7 +6,9 @@ export const Carinfo = (props) => {
   const { store, actions } = useContext(Context);
   return (
     <div>
+      <p>Car Year: {props?.data.year}</p>
       <p>Car Make: {props?.data.make}</p>
+      <p>Car Model: {props?.data.model}</p>
     </div>
   );
 };
