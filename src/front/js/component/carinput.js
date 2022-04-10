348e92967d744aa0f71e9cb4fb0b9af2c1a5f1e5
Keenan <kkan68@gmail.com>
2022-04-10 13:18:54 +0000

    added controlled inputs and mileage entry to form page

--- src/front/js/component/carinput.js
+++ src/front/js/component/carinput.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 
 export const Carinput = () => {
+  const {store, actions} = useContext(Context);
+  const [vin, setVIN] = useState("")
+  const [model, setModel] = useState("")
+  const [mileage, setMileage] = useState("")
+
   return (
     <div>
       <form>
@@ -9,7 +14,13 @@ export const Carinput = () => {
           <label for="vin" className="form-label">
             Car VIN
           </label>
-          <input className="form-control" name="vin"></input>
+          <input className="form-control" name="vin" value={vin}></input>
+        </div>
+          <div className="form-container col-3 m-auto">
+            <label for="vin" className="form-label">
+            Mileage
+          </label>
+          <input className="form-control" name="vinmileage" value={mileage}></input>
         </div>
       </form>
       <h5>or</h5>
@@ -18,7 +29,11 @@ export const Carinput = () => {
           <label for="models" className="form-label">
             Car Year, Make, and Model
           </label>
-          <input className="form-control" name="models"></input>
+          <input className="form-control" name="models" value={model}></input>
+          <label for="models" className="form-label">
+            Mileage
+          </label>
+          <input className="form-control" name="modelsmileage" value={mileage}></input>
         </div>
       </form>
     </div>
