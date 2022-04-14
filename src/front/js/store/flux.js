a555080d32b9807d02e95051fa08fd86055d9df8
Keenan <kkan68@gmail.com>
2022-04-14 14:36:51 +0000

    added more flux functions

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -474,6 +474,25 @@ const getState = ({ getStore, getActions, setStore }) => {
           setStore({ carmaintenance: payload.data });
         }
       },
+      loadInformationmodel: async (model, mileage) => {
+        const store = getStore();
+        const actions = getActions();
+        const session = actions.getCurrentSession();
+        const options = {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        };
+        const response = await fetch(
+          f`http://api.carmd.com/v3.0/maint?vin=${vin}&mileage=${mileage}`,
+          options
+        );
+        if (response.status === 200) {
+          const payload = await response.json();
+          setStore({ carmaintenance: payload.data });
+        }
+      },
     },
   };
 };
